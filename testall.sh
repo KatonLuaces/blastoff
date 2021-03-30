7db8bbb4a04a1b079584c8c18e43e5f145c7a278
Michael Jan <michaeljancontact@gmail.com>
2021-03-29 22:49:42 -0400

    link graphblas in testall.sh

--- testall.sh
+++ testall.sh
@@ -90,7 +90,7 @@ Check() {
     generatedfiles="$generatedfiles ${basename}.ll" &&
      "$BLASTOFF -l" "$1" ">" "${basename}.ll" &&
      "$LLC" "-relocation-model=pic" "${basename}.ll" ">" "${basename}.s" &&
-     "$CC" "-o" "${basename}.exe" "${basename}.s" "printbig.o" &&
+     "$CC" "-o" "${basename}.exe" "${basename}.s" "printbig.o" "-lgraphblas" &&
      "./${basename}.exe" > "${basename}.out" &&
     Compare ${basename}.ll ${reffile}.out ${basename}.diff
 
