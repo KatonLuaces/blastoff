db5f38bed1bce4dbb704f163dc216223916e0a3b
Jason Kao <jason.kao@columbia.edu>
2021-03-29 21:11:25 -0400

    unnecessary file

--- helloworld.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-make 
-
-./blastoff.native helloworld.bl > helloworld.ll \
-    && llc -relocation-model=pic helloworld.ll > helloworld.s \
-    && cc  -o  helloworld.exe  helloworld.s  graphblas.o \
-    && ./helloworld.exe
