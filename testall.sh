edd58e85af09bd78a04553a920c192871b8b306d
Michael Jan <michaeljancontact@gmail.com>
2021-04-18 18:57:19 -0400

    fixed print and helloworld test

--- testall.sh
+++ testall.sh
@@ -52,7 +52,7 @@ SignalError() {
 Compare() {
     generatedfiles="$generatedfiles $3"
     echo diff -b $1 $2 ">" $3 1>&2
-    diff -b "$1" "$2" > "$3" 2>&1 || {
+    diff -b "$1" "$2" > "\"$3\"" 2>&1 || {
 	SignalError "$1 differs"
 	echo "FAILED $1 differs from $2" 1>&2
     }
