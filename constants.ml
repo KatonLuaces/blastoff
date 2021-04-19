35d8761b29df53fad9b9a2644254fb5cc9833f3a
KatonLuaces <katon.luaces@gmail.com>
2021-04-19 10:20:12 -0400

    Fix selection test and generator test

--- constants.ml
+++ constants.ml
@@ -1,2 +1,2 @@
 let rings  = [("arith", 0) ; ("logic", 1); ("maxmin", 2)]
-let functions = [("I", ["n"]); ("Zero", ["n";"m"]); ("Range", ["n"]); ("print", ["e"]); ("toString", ["e"])]
+let functions = [("I", ["n"]); ("Zero", ["m"]); ("Range", ["n"]); ("print", ["e"]); ("toString", ["e"])]
