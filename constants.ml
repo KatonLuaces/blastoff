1119b008c40e612ec71450e0c892b9aeb7e67956
Michael Jan <michaeljancontact@gmail.com>
2021-04-19 13:24:57 -0400

    add I, Zero, and range matrix generators

--- constants.ml
+++ constants.ml
@@ -1,2 +1,2 @@
 let rings  = [("arith", 0) ; ("logic", 1); ("maxmin", 2)]
-let functions = [("I", ["n"]); ("Zero", ["n";"m"]); ("Range", ["n"]); ("print", ["e"]); ("toString", ["e"])]
+let functions = [("I", ["n"]); ("Zero", ["d"]); ("range", ["n"]); ("print", ["e"]); ("toString", ["e"])]
