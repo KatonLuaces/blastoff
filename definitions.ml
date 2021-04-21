9c1fdbfddd3b1cb2e2e492e90619ad60193169b1
KatonLuaces <katon.luaces@gmail.com>
2021-04-21 15:58:49 -0400

    Add association list of definitions

--- definitions.ml
+++ definitions.ml
@@ -8,7 +8,7 @@ let blastoff_module = L.create_module context "BLAStoff"
 let rings  = [("arith", 0) ; ("logic", 1); ("maxmin", 2)]
 let functions = [("I", ["n"]); ("Zero", ["d"]); ("range", ["n"]); ("print", ["e"]); ("toString", ["e"])]
 
-type built_in = {name: string; args: L.lltype list}
+type built_in = {name: string; ret: L.lltype; args: L.lltype list}
 
 let i32_t = L.i32_type context
 let matrix_t =
@@ -17,25 +17,27 @@ let matrix_t =
     | None-> raise (Failure "matrix type implementation not found")
     | Some t -> t)
 
-let built_ins: built_in list = [
-  {name="matrix_create"; args=[matrix_t]};
-  {name="matrix_create_identity";args=[matrix_t]};
-  {name="matrix_create_zero";args=[matrix_t]};
-  {name="matrix_create_range";args=[matrix_t]};
-  {name="matrix_print";args=[matrix_t]};
-  {name="matrix_tostring";args=[matrix_t]};
-  {name="change_ring";args=[i32_t]};
-  {name="matrix_setelem";args=[matrix_t; i32_t; i32_t; i32_t]};
-  {name="matrix_mul";args=[matrix_t; matrix_t]};
-  {name="matrix_conv";args=[matrix_t; matrix_t]};
-  {name="matrix_elmul";args=[matrix_t; matrix_t]};
-  {name="matrix_eladd";args=[matrix_t; matrix_t]};
-  {name="matrix_extract";args=[matrix_t; matrix_t; matrix_t; matrix_t; matrix_t;]};
-  {name="matrix_insert";args=[matrix_t; matrix_t; matrix_t; matrix_t; matrix_t; matrix_t;]};
-  {name="matrix_bool";args=[matrix_t]}
+let built_in_defs: built_in list = [
+  {name="matrix_create"; ret=matrix_t; args=[matrix_t]};
+  {name="matrix_create_identity"; ret=matrix_t; args=[matrix_t]};
+  {name="matrix_create_zero"; ret=matrix_t; args=[matrix_t]};
+  {name="matrix_create_range"; ret=matrix_t; args=[matrix_t]};
+  {name="matrix_print"; ret=i32_t; args=[matrix_t]};
+  {name="matrix_tostring"; ret=matrix_t; args=[matrix_t]};
+  {name="change_ring"; ret=i32_t; args=[i32_t]};
+  {name="matrix_setelem"; ret=i32_t; args=[matrix_t; i32_t; i32_t; i32_t]};
+  {name="matrix_mul"; ret=matrix_t; args=[matrix_t; matrix_t]};
+  {name="matrix_conv"; ret=matrix_t; args=[matrix_t; matrix_t]};
+  {name="matrix_elmul"; ret=matrix_t; args=[matrix_t; matrix_t]};
+  {name="matrix_eladd"; ret=matrix_t; args=[matrix_t; matrix_t]};
+  {name="matrix_extract"; ret=matrix_t; args=[matrix_t; matrix_t; matrix_t; matrix_t; matrix_t;]};
+  {name="matrix_insert"; ret=matrix_t; args=[matrix_t; matrix_t; matrix_t; matrix_t; matrix_t; matrix_t;]};
+  {name="matrix_bool"; ret=i32_t; args=[matrix_t]}
 ]
 
-(*let builtin_fun_types : (L.llvalue * func_decl) StringMap.t =*)
+let create_fun_type fdef = L.function_type fdef.ret (Array.of_list fdef.args)
+
+let built_ins = List.map (fun fdef -> (fdef.name, create_fun_type fdef)) built_in_defs 
 
 let matrix_create_t = L.function_type matrix_t [| i32_t; i32_t |]
 let matrix_create_f = L.declare_function "matrix_create" matrix_create_t blastoff_module
