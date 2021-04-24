0a74ab619451a090d946ef4df9895d193d20e097
KatonLuaces <katon.luaces@gmail.com>
2021-04-24 10:55:51 -0400

    Clean ast.ml

--- ast.ml
+++ ast.ml
@@ -71,32 +71,51 @@ let string_of_op = function
   | Geq -> ">="
   | Exponent -> "^"
   | Concat -> ":"
+;;
 
 let string_of_mat lit_to_string m =
-   let string_of_row row =
-     String.concat "," (List.fold_left (fun acc lit ->  (lit_to_string lit) :: acc) [] row)
-    in
-    "[" ^ (String.concat ";" (List.fold_left (fun acc row -> (string_of_row row) :: acc) []  m)) ^ "]"
+  let string_of_row row =
+    String.concat "," (List.fold_left (fun acc lit -> lit_to_string lit :: acc) [] row)
+  in
+  "["
+  ^ String.concat ";" (List.fold_left (fun acc row -> string_of_row row :: acc) [] m)
+  ^ "]"
+;;
 
 let string_of_graph g =
-   let string_of_edge (v1,v2) = (string_of_int v1) ^ "->" ^ (string_of_int v2) in
-   "[" ^ (String.concat ";" (List.map string_of_edge g)) ^ "]"
+  let string_of_edge (v1, v2) = string_of_int v1 ^ "->" ^ string_of_int v2 in
+  "[" ^ String.concat ";" (List.map string_of_edge g) ^ "]"
+;;
 
 let rec string_of_expr = function
   | Id s -> s
-  | Binop (e1, o, e2) -> string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Binop (e1, o, e2) ->
+    string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
   | Unop (o, e) -> string_of_e_with_uop e o
   | Assign (e1, e2) -> string_of_expr e1 ^ " = " ^ string_of_expr e2
   | IdAssign (s, e) -> s ^ " = " ^ string_of_expr e
   | Call (f, el) -> f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
-  | UnkMatLit m -> string_of_mat (fun lit -> match lit with
-        IntLit ilit -> string_of_int ilit| FloatLit flit -> string_of_float flit) m
+  | UnkMatLit m ->
+    string_of_mat
+      (fun lit ->
+        match lit with
+        | IntLit ilit -> string_of_int ilit
+        | FloatLit flit -> string_of_float flit)
+      m
   | IntMatLit m -> string_of_mat string_of_int m
   | GraphLit g -> string_of_graph g
   | StringLit s -> "'" ^ s ^ "'"
   | FloatMatLit m -> string_of_mat string_of_float m
-  | Selection (e, args) -> (string_of_expr e) ^ "[" ^ String.concat ", " (List.map string_of_expr args) ^ "]"
-  | SelectAssign (s, args, e) -> s ^ "[" ^ String.concat ", " (List.map string_of_expr args) ^ "]" ^ " = " ^ string_of_expr e
+  | Selection (e, args) ->
+    string_of_expr e ^ "[" ^ String.concat ", " (List.map string_of_expr args) ^ "]"
+  | SelectAssign (s, args, e) ->
+    s
+    ^ "["
+    ^ String.concat ", " (List.map string_of_expr args)
+    ^ "]"
+    ^ " = "
+    ^ string_of_expr e
+
 and string_of_e_with_uop e =
   let str_expr = string_of_expr e in
   function
@@ -105,6 +124,7 @@ and string_of_e_with_uop e =
   | Transp -> str_expr ^ "^T"
   | Plusreduce -> "+%" ^ str_expr
   | Mulreduce -> "*%" ^ str_expr
+;;
 
 let rec string_of_stmt = function
   | Semiring ring -> "#" ^ ring ^ "\n"
@@ -115,6 +135,7 @@ let rec string_of_stmt = function
   | If (e, s1, s2) ->
     "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
   | While (e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+;;
 
 let string_of_func func =
   "def "
@@ -125,8 +146,10 @@ let string_of_func func =
   ^ "{\n"
   ^ String.concat "" (List.map string_of_stmt func.body)
   ^ "}\n"
+;;
 
 let string_of_program (funcs, stmts) =
   String.concat "" (List.map string_of_func funcs)
   ^ "\n"
   ^ String.concat "" (List.map string_of_stmt stmts)
+;;
