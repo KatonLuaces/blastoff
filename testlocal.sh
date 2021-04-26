2f6ab97ada546dc30290fa15fcfaf781c315e551
Michael Jan <michaeljancontact@gmail.com>
2021-04-26 18:55:03 -0400

    remove testlocal.sh

--- testlocal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./blastoff.native $1 > blastoff.ll \
-    && llc -relocation-model=pic blastoff.ll > blastoff.s \
-    &&  cc -o blastoff.exe blastoff.s graphblas.o -lgraphblas \
-    && ./blastoff.exe
