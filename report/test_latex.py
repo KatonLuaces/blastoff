af3b5ad00c9ce1e3de289de895c57faead9c0bbf
jfisher18 <jf3148@columbia.edu>
2021-04-26 21:44:43 -0400

    Better testing

--- report/test_latex.py
+++ report/test_latex.py
@@ -6,13 +6,7 @@ TEST_DIR = '../tests/'
 output = ""
 
 for t in sorted(os.listdir(TEST_DIR)):
-    if not t.endswith('.bl'):
-        continue
-    output += '\subsubsection{' + t.replace('_', '\_') + '}\n'
-    output += '\\begin{lstlisting}\n'
-    with open(TEST_DIR + t) as f:
-        output += f.read()
-    output += '\\end{lstlisting}\n'
+    output += '\\verb='+t+'=:\n\lstinputlisting{../tests/'+t+'}\n'
 
 # print(output)
 with open('appendix_test.tex', 'w') as f:
