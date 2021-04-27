6cb2c2296d4ccc426c32412e71ad91320823d295
Jason Kao <jason.kao@columbia.edu>
2021-04-26 21:32:41 -0400

    errors

--- report/test_latex.py
+++ report/test_latex.py
@@ -2,18 +2,18 @@
 
 import os
 
-TEST_DIR = '../tests/'
+TEST_DIR = "../tests/"
 output = ""
 
 for t in sorted(os.listdir(TEST_DIR)):
-    if not t.endswith('.bl'):
+    if t == ".DS_Store":
         continue
-    output += '\subsubsection{' + t.replace('_', '\_') + '}\n'
-    output += '\\begin{lstlisting}\n'
+    output += "\subsubsection{" + t.replace("_", "\_") + "}\n"
+    output += "\\begin{lstlisting}\n"
     with open(TEST_DIR + t) as f:
         output += f.read()
-    output += '\\end{lstlisting}\n'
+    output += "\\end{lstlisting}\n"
 
 # print(output)
-with open('appendix_test.tex', 'w') as f:
+with open("appendix_test.tex", "w") as f:
     f.write(output)
