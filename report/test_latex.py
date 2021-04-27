88cf75c5217dd2e793b3e8ac0d0ee61640f60faa
jfisher18 <jf3148@columbia.edu>
2021-04-26 21:51:05 -0400

    Test stuff

--- report/test_latex.py
+++ report/test_latex.py
@@ -8,11 +8,8 @@ output = ""
 for t in sorted(os.listdir(TEST_DIR)):
     if t == ".DS_Store":
         continue
-    output += "\subsubsection{" + t.replace("_", "\_") + "}\n"
-    output += "\\begin{lstlisting}\n"
-    with open(TEST_DIR + t) as f:
-        output += f.read()
-    output += "\\end{lstlisting}\n"
+    output += t.replace("_", "\_") + ":"
+    output += '\lstinputlisting{../tests/'+t+'}\n'
 
 # print(output)
 with open("appendix_test.tex", "w") as f:
